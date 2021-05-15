9c742e6829e32693e959362794ab116cb2466711
Debbie <dbarros64@gmail.com>
2021-05-15 16:55:27 -0700

    added context api and replaced all of the redux files

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: "AIzaSyB3-NDr-uGQ-VOVKQFvWeEBAoIbGTV55cM",
+  authDomain: "crown-clothdb.firebaseapp.com",
+  projectId: "crown-clothdb",
+  storageBucket: "crown-clothdb.appspot.com",
+  messagingSenderId: "67184053",
+  appId: "1:67184053:web:8d164ad1a519c3c06765fc",
+  measurementId: "G-HQB6KCTK1B"
 };
 
 firebase.initializeApp(config);
